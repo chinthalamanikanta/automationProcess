b74820f508ae943aaaf81cd84003dad9772e68d8
MTL1026 <chm80076@gmail.com>
2025-02-03 12:36:53 +0530

    New second

--- src/main/java/com/accesshr/emsbackend/EmsBackendApplication.java
+++ src/main/java/com/accesshr/emsbackend/EmsBackendApplication.java
@@ -1,8 +1,10 @@
 package com.accesshr.emsbackend;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
@@ -13,4 +15,10 @@ public class EmsBackendApplication {
         SpringApplication.run(EmsBackendApplication.class, args);
     }
 
+    @Bean
+    public CommandLineRunner commandLineRunner() {
+        return args -> {
+            System.out.println("Hello World");
+        };
+    }
 }
